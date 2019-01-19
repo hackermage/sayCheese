c47d575c68484a0382ee409461d7d9f282099a3c
Xingyu Li <xli279@ucsc.edu>
2019-01-19 15:30:51 -0800

    modify readme and feedforward

--- README.md
+++ README.md
@@ -1,43 +1,27 @@
-<img src='http://www.albertpumarola.com/images/2018/GANimation/face1_cyc.gif' align="right" width=90>
-
+<!-- <img src='http://www.albertpumarola.com/images/2018/GANimation/face1_cyc.gif' align="right" width=90>
+ -->
 # GANimation: Anatomically-aware Facial Animation from a Single Image
 ### [[Project]](http://www.albertpumarola.com/research/GANimation/index.html)[ [Paper]](https://arxiv.org/abs/1807.09251) 
-Official implementation of [GANimation](http://www.albertpumarola.com/research/GANimation/index.html). In this work we introduce a novel GAN conditioning scheme based on Action Units (AU) annotations, which describe in a continuous manifold the anatomical facial movements defining a human expression. Our approach permits controlling the magnitude of activation of each AU and combine several of them. For more information please refer to the [paper](https://arxiv.org/abs/1807.09251).
+Official implementation of [GANimation](http://www.albertpumarola.com/research/GANimation/index.html). In this work we introduce a novel GAN conditioning scheme based on Action Units (AU) annotations, which describe in a continuous manifold the anatomical facial movements defining a human expression. Our approach permits controlling the magnitude of activation of each AU and combine several of them. 
 
-This code was made public to share our research for the benefit of the scientific community. Do NOT use it for immoral purposes.
-
-![GANimation](http://www.albertpumarola.com/images/2018/GANimation/teaser.png)
+<!-- This code was made public to share our research for the benefit of the scientific community. Do NOT use it for immoral purposes.
+ -->
 
 ## Prerequisites
-- Install PyTorch (version 0.3.1), Torch Vision and dependencies from http://pytorch.org
+- Install PyTorch (we use version 1.0.0), Torch Vision and dependencies from http://pytorch.org
 - Install requirements.txt (```pip install -r requirements.txt```)
 
-## Data Preparation
-The code requires a directory containing the following files:
-- `imgs/`: folder with all image
-- `aus_openface.pkl`: dictionary containing the images action units.
-- `train_ids.csv`: file containing the images names to be used to train.
-- `test_ids.csv`: file containing the images names to be used to test.
-
-An example of this directory is shown in `sample_dataset/`.
-
-To generate the `aus_openface.pkl` extract each image Action Units with [OpenFace](https://github.com/TadasBaltrusaitis/OpenFace/wiki/Action-Units) and store each output in a csv file the same name as the image. Then run:
-```
-python data/prepare_au_annotations.py
-```
-
 ## Run
-To train:
+
+First, put the pretrained model(s) anywhere you like, they are files named net_epoch_#epoch_id_G.pth and net_epoch_#epoch_id_D.pth
+
+To run demo:
 ```
-bash launch/run_train.sh
-```
-To test:
-```
-python test --input_path path/to/img
+python feedforwar.py --model_path path/to/pretrained_model --load_epoch index_of_epoch_for_the_model --img_path path/to/img
 ```
 
 ## Citation
-If you use this code or ideas from the paper for your research, please cite our paper:
+Our idea and work are based on Albert Pumarola's [GANimation](http://www.albertpumarola.com/images/2018/GANimation/teaser.png). For more information about the model please refer to the [paper](https://arxiv.org/abs/1807.09251).
 ```
 @inproceedings{pumarola2018ganimation,
     title={GANimation: Anatomically-aware Facial Animation from a Single Image},
--- feedforward.py
+++ feedforward.py
@@ -120,39 +120,98 @@ def main():
 
     convertor = feedFoward(pathG, pathD)
 
-    # set expression
-    expressions = np.ndarray((5,17), dtype = np.float)
-
     # define the target AU, for test use only
-    target_AU = np.array([0.25, 0.11, 0.2 , 0.16, 1.92, 1.03, 0.3 , 2.15, 2.88, 1.61, 0.03, 0.09, 0.16, 0.11, 2.25, 0.37, 0.05], dtype = np.float)
+    # target_AU = np.array(
+    # [[0.2 , 0.12, 0.1 , 0.24, 0.05, 0.12, 0.08, 0.11, 0.19, 0.22, 0.1 , 0.09, 0.15, 0.07, 0.11, 0.16, 0.03],
+    #  [0.4 , 0.1 , 0.28, 0.45, 0.46, 0.32, 0.36, 1.61, 0.72, 0.45, 0.34, 0.28, 0.29, 0.09, 0.34, 0.3 , 0.07],
+    #  [0.51, 0.37, 0.17, 1.53, 0.09, 0.16, 0.1 , 0.21, 0.21, 0.21, 0.19, 0.15, 0.2 , 0.09, 0.23, 0.29, 0.04],
+    #  [1.02, 0.34, 2.  , 0.54, 0.32, 0.66, 0.25, 0.6 , 0.38, 0.56, 0.33, 0.33, 0.3 , 0.13, 0.28, 0.36, 0.09],
+    #  [0.27, 0.17, 0.08, 0.26, 0.51, 0.38, 0.12, 0.57, 1.46, 0.56, 0.04, 0.04, 0.16, 0.06, 1.22, 0.44, 0.03],
+    #  [0.27, 0.2 , 0.14, 0.26, 0.07, 0.19, 0.07, 0.23, 0.2 , 0.23, 0.16, 0.1 , 0.2 , 0.06, 0.43, 1.19, 0.05],
+    #  [1.46, 0.76, 0.5 , 0.79, 1.15, 0.95, 0.2 , 1.48, 1.94, 1.04, 0.11, 0.13, 0.3 , 0.09, 1.79, 0.53, 0.06],
+    #  [0.67, 0.35, 0.34, 0.51, 0.47, 0.7 , 0.26, 0.84, 0.54, 0.5 , 0.4 , 0.26, 0.33, 0.14, 1.14, 2.12, 0.09],
+    #  [1.52, 0.8 , 0.24, 0.41, 0.15, 0.21, 0.07, 0.23, 0.3 , 0.32, 0.23, 0.19, 0.32, 0.09, 0.25, 0.29, 0.05],
+    #  [2.07, 1.32, 0.49, 1.72, 0.4 , 0.38, 0.11, 0.56, 0.56, 0.65, 0.33, 0.54, 0.42, 0.2 , 0.39, 0.39, 0.06],
+    #  [0.38, 0.13, 0.27, 0.32, 0.4 , 1.5 , 0.27, 0.25, 0.45, 0.31, 0.23, 0.15, 0.37, 0.12, 0.28, 0.37, 0.04],
+    #  [0.92, 0.34, 0.75, 0.66, 1.47, 2.64, 0.69, 0.98, 0.84, 0.61, 0.56, 0.72, 0.91, 0.28, 0.47, 0.34, 0.05],
+    #  [0.23, 0.07, 0.18, 0.16, 1.04, 1.66, 0.28, 0.68, 1.64, 0.96, 0.07, 0.13, 0.31, 0.16, 0.82, 0.34, 0.04],
+    #  [0.17, 0.1 , 0.1 , 0.17, 1.11, 0.48, 0.15, 1.35, 2.3 , 1.19, 0.02, 0.05, 0.13, 0.08, 1.7 , 0.3 , 0.03],
+    #  [0.26, 0.13, 0.13, 0.22, 0.3 , 0.26, 0.12, 0.4 , 0.96, 1.37, 0.07, 0.27, 0.24, 0.21, 0.21, 0.25, 0.04],
+    #  [0.65, 0.29, 0.33, 0.49, 0.67, 0.44, 0.27, 0.8 , 0.56, 0.74, 0.62, 1.73, 0.6 , 0.44, 0.11, 0.38, 0.08],
+    #  [0.25, 0.11, 0.2 , 0.16, 1.92, 1.03, 0.3 , 2.15, 2.88, 1.61, 0.03, 0.09, 0.16, 0.11, 2.25, 0.37, 0.05],
+    #  [0.34, 0.1 , 0.48, 0.23, 1.44, 0.89, 0.68, 1.84, 1.63, 1.7 , 0.22, 0.63, 0.34, 0.32, 0.53, 0.32, 0.07],
+    #  [0.19, 0.06, 0.16, 0.14, 1.59, 1.9 , 0.27, 1.32, 2.38, 0.92, 0.04, 0.03, 0.2 , 0.08, 1.98, 0.45, 0.04],
+    #  [0.32, 0.06, 0.49, 0.17, 2.37, 2.65, 0.71, 2.16, 2.65, 1.4 , 0.08, 0.07, 0.32, 0.13, 2.29, 0.55, 0.05]], dtype = np.float)
 
-    # find original AU of input image using discrinator of GANimation, for test use only
+    target_AU = np.array(
+    [[0.3 , 0.18, 0.1 , 0.25, 0.8 , 0.46, 0.13, 0.94, 1.89, 0.87, 0.03, 0.05, 0.15, 0.07, 1.49, 0.39, 0.03],
+     [0.22, 0.09, 0.1 , 0.17, 1.46, 1.83, 0.27, 1.16, 2.23, 0.9 , 0.05, 0.04, 0.22, 0.08, 1.78, 0.44, 0.04],
+     [2.08, 1.28, 0.48, 1.6 , 0.51, 0.43, 0.12, 0.7 , 0.75, 0.77, 0.31, 0.53, 0.43, 0.2 , 0.55, 0.44, 0.07],
+     [0.24, 0.06, 0.25, 0.14, 2.31, 2.31, 0.57, 2.15, 2.8 , 1.43, 0.05, 0.07, 0.26, 0.12, 2.35, 0.51, 0.05],
+     [0.46, 0.29, 0.21, 0.38, 0.21, 0.39, 0.14, 0.47, 0.35, 0.33, 0.27, 0.15, 0.26, 0.09, 0.81, 1.71, 0.07],
+     [0.19, 0.12, 0.1 , 0.23, 0.05, 0.15, 0.08, 0.11, 0.19, 0.21, 0.1 , 0.09, 0.16, 0.06, 0.16, 0.28, 0.04],
+     [0.86, 0.24, 1.63, 0.36, 1.64, 2.21, 0.59, 1.66, 1.88, 1.1 , 0.14, 0.12, 0.32, 0.13, 1.71, 0.54, 0.07],
+     [0.31, 0.15, 0.16, 0.21, 1.6 , 0.71, 0.22, 1.92, 2.69, 1.47, 0.03, 0.08, 0.16, 0.1 , 2.09, 0.34, 0.04],
+     [0.59, 0.27, 0.33, 0.47, 0.6 , 0.39, 0.27, 0.78, 0.52, 0.75, 0.61, 1.68, 0.54, 0.43, 0.1 , 0.39, 0.08],
+     [0.32, 0.11, 0.22, 0.25, 0.58, 1.56, 0.27, 0.34, 0.83, 0.5 , 0.15, 0.14, 0.35, 0.14, 0.44, 0.38, 0.04],
+     [0.41, 0.1 , 0.28, 0.44, 0.45, 0.32, 0.35, 1.57, 0.73, 0.42, 0.32, 0.25, 0.28, 0.08, 0.41, 0.33, 0.07],
+     [0.24, 0.12, 0.12, 0.22, 0.29, 0.26, 0.11, 0.36, 1.  , 1.27, 0.06, 0.22, 0.23, 0.19, 0.26, 0.26, 0.03],
+     [1.  , 0.33, 1.94, 0.54, 0.3 , 0.66, 0.25, 0.57, 0.34, 0.52, 0.33, 0.32, 0.3 , 0.13, 0.26, 0.37, 0.09],
+     [0.35, 0.11, 0.38, 0.23, 1.36, 0.91, 0.6 , 1.67, 1.63, 1.66, 0.2 , 0.56, 0.34, 0.3 , 0.51, 0.31, 0.07],
+     [1.49, 0.81, 0.23, 0.45, 0.14, 0.22, 0.07, 0.21, 0.28, 0.29, 0.23, 0.18, 0.31, 0.08, 0.25, 0.29, 0.05],
+     [0.5 , 0.37, 0.17, 1.52, 0.09, 0.18, 0.1 , 0.21, 0.22, 0.22, 0.19, 0.15, 0.21, 0.09, 0.23, 0.3 , 0.04],
+     [0.62, 0.2 , 0.64, 0.43, 1.08, 1.89, 0.53, 0.91, 0.88, 0.7 , 0.4 , 0.56, 0.6 , 0.25, 0.48, 0.44, 0.06],
+     [0.27, 0.15, 0.17, 0.36, 0.13, 0.25, 0.12, 0.27, 0.34, 0.39, 0.15, 0.18, 0.2 , 0.1 , 0.24, 0.42, 0.04],
+     [0.31, 0.15, 0.17, 0.24, 0.9 , 0.57, 0.21, 1.12, 1.8 , 1.1 , 0.07, 0.17, 0.19, 0.13, 1.19, 0.39, 0.04],
+     [1.52, 0.84, 0.55, 1.08, 0.28, 0.31, 0.11, 0.5 , 0.43, 0.51, 0.32, 0.43, 0.37, 0.15, 0.35, 0.44, 0.06],
+     [0.29, 0.1 , 0.28, 0.18, 1.89, 1.73, 0.39, 1.84, 2.61, 1.3 , 0.05, 0.08, 0.22, 0.11, 2.12, 0.44, 0.04],
+     [0.92, 0.35, 0.58, 0.68, 1.43, 2.54, 0.64, 0.91, 0.8 , 0.58, 0.6 , 0.8 , 0.98, 0.3 , 0.39, 0.35, 0.05]], dtype = np.float)
+    # target_AU = np.array([0.25, 0.11, 0.2 , 0.16, 1.92, 1.03, 0.3 , 2.15, 2.88, 1.61, 0.03, 0.09, 0.16, 0.11, 2.25, 0.37, 0.05], dtype = np.float)
+
+    # # find original AU of input image using discrinator of GANimation, for test use only
     out_real, out_aux = convertor.FindAU(real_face) # out_aux is the AU value from D 
     original_AU = out_aux.data.numpy()
 
-    # interpolate between original_AU and the target_AU, for test only
-    c = (original_AU - target_AU)/4
-    for i in range(5):
-       expressions[i] = c * i + target_AU
+    # set expression
+    row_num = np.shape(target_AU)[0]
+
+    expression_num = 5
+    expressions = np.ndarray((expression_num, row_num, 17), dtype = np.float)
+
+    # # interpolate between original_AU and the target_AU, for test only
+    o_AU = np.repeat([original_AU], row_num, axis = 0)
+    assert np.shape(o_AU) == np.shape(target_AU), "shape not match"
+
+    c = (o_AU - target_AU)/(expression_num - 1)
+    for i in range(expression_num):
+        expressions[i] = c * i + target_AU
 
     # run model for above expressions
-    result = np.zeros((1, img_raw.shape[1]*4, 3))
-    for i in range(5):
-        processed_face, maskA = convertor.Foward(real_face, expressions[i])
-        new_img, mask, rotate, new_maskA = face.face_place_back(img_raw, processed_face, face_origin_pos, 
-                                                                maskA_test=True, maskA = maskA)
-
-        # handle maskA to image for dispalying
-        maskA_t = np.expand_dims(new_maskA, axis=2)
-        maskA_t = (maskA_t*254.0).astype(np.uint8)
-        maskA_t = np.repeat(maskA_t, 3, axis=-1)
-
-        # wrap up results
-        current_result = np.hstack((img_raw, rotate, maskA_t, new_img))
+    result = np.zeros((1, img_raw.shape[1] * (expression_num + 1), 3))
+    for j in range(row_num):
+        current_result = img_raw
+        for i in range(expression_num):
+            processed_face, maskA = convertor.Foward(real_face, expressions[i, j])
+            new_img, mask, rotate, new_maskA = face.face_place_back(img_raw, processed_face, face_origin_pos, 
+                                                                    maskA_test=True, maskA = maskA)
+            current_result = np.hstack((current_result, new_img))
         result = np.vstack((result, current_result))
-        #print(np.shape(processed_face), np.shape(maskA), maskA.dtype)
 
-    result  = cv2.resize(result,(0,0), fx=0.5, fy=0.5)
+    #     processed_face, maskA = convertor.Foward(real_face, expressions[i])
+    #     new_img, mask, rotate, new_maskA = face.face_place_back(img_raw, processed_face, face_origin_pos, 
+    #                                                             maskA_test=True, maskA = maskA)
+
+    #     # handle maskA to image for dispalying
+    #     maskA_t = np.expand_dims(new_maskA, axis=2)
+    #     maskA_t = (maskA_t*254.0).astype(np.uint8)
+    #     maskA_t = np.repeat(maskA_t, 3, axis=-1)
+
+    #     # wrap up results
+    #     current_result = np.hstack((img_raw, rotate, maskA_t, new_img))
+    #     result = np.vstack((result, current_result))
+    #     #print(np.shape(processed_face), np.shape(maskA), maskA.dtype)
+
+    #result  = cv2.resize(result,(0,0), fx=0.5, fy=0.5)
 
     timestamp = calendar.timegm(time.gmtime())
     image_out_name = "./results/processedface_"+image_name+"-"+str(timestamp)+".jpg"
