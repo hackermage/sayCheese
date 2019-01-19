db0165573cef988e6b528ec22ac623b1516ac9ab
JSchmidt <J.boaz.schmidt@gmail.com>
2019-01-19 11:37:41 -0800

    Fix feedforward input

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-from feedforward import feedForward
+import feedforward.feedFoward
 app = Flask(__name__)
 api = Api(app)
 
